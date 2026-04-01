915007a92b73c45e10765492e7fd8b23de0831c3
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-31 20:58:40 -0500

    style: js-beautify formatting (src/index.mjs)

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,12 @@
 let hadExports = true;
 const isObject = x => typeof x === 'object' && x !== null;
-if(!isObject(globalThis.exports)){
+if (!isObject(globalThis.exports)) {
   hadExports = false;
   globalThis.exports = Object(globalThis.exports);
 }
 await import('./index.js');
 const module = exports;
-if(!hadExports)delete globalThis.exports;
+if (!hadExports) delete globalThis.exports;
 export const cookieStore = module.cookieStore;
 export const CookieStore = module.CookieStore;
 export const CookieChangeEvent = module.CookieChangeEvent;
